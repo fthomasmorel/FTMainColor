346a0dc6b5d77760f15f261d020f366ef91e56cb
fthomasmorel <fthomasm@insa-rennes.fr>
2015-07-29 21:02:27 +0200

    Initial Commit

---
+++ FTMainColor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FTMainColor
+//
+//  Created by Florent TM on 29/07/2015.
+//  Copyright © 2015 Florent THOMAS-MOREL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
